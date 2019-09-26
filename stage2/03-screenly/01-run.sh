a3fa23957a11bdaa73894735c84456d12d1b86dc
Rusko124 <pavel.lvl@yandex.ru>
2019-09-26 11:06:57 +0600

    Edits: default assets

--- stage2/03-screenly/01-run.sh
+++ stage2/03-screenly/01-run.sh
@@ -29,17 +29,8 @@ on_chroot << EOF
   find /usr/share/locale -type f ! -name 'en' ! -name 'de*' ! -name 'es*' ! -name 'ja*' ! -name 'fr*' ! -name 'zh*' -delete
   find /usr/share/locale -mindepth 1 -maxdepth 1 ! -name 'en*' ! -name 'de*' ! -name 'es*' ! -name 'ja*' ! -name 'fr*' ! -name 'zh*' -exec rm -r {} \;
 
-  HOME=/home/pi python /home/pi/screenly/server.py &
-
-  # Wait for server to start
-  /tmp/wait-for-it.sh --host=127.0.0.1 --port=8080
-
   # Adds default assets
-  /home/pi/screenly/bin/prepare_device_for_imaging.sh
+  HOME=/home/pi python -c "import server;server.add_default_assets();server.settings['default_assets']=True;server.settings.save()"
 
-  pkill -f server.py
-  sleep 5
   chown -R pi:pi /home/pi
-
-  rm /tmp/wait-for-it.sh
 EOF
